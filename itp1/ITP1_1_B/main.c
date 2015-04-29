f12b9530a263ee691de02ed62ece6b76ae6b2c4c
Koji Ota <afterjnih@gmail.com>
2015-04-29 16:08:21 +0900

    slove ITP1_1_B using java and c

---
+++ itp1/ITP1_1_B/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ITP1_1_B
+
+#include <stdio.h>
+#include <math.h>
+
+int main(int argc, const char * argv[]) {
+    int i, input;
+    scanf("%d", &input);
+    i = (int)pow(input, 3.0);
+    printf("%d\n", i);
+    return 0;
+}
